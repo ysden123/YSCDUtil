56dc9eabc28be0a7336ec6a40d26014ce5d0c167
ysden123 <ysden123@gmail.com>
2015-12-23 20:09:06 +0200

    back to Java & gradle

---
+++ src/main/java/com/stulsoft/yscdutil/YSCDUtilMain.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2015, Yuriy Stul. All rights reserved
+ */
+package com.stulsoft.yscdutil;
+
+/**
+ * @author Yuriy Stul
+ *
+ */
+public class YSCDUtilMain {
+
+	/**
+	 * @param args
+	 *            input parameters
+	 */
+	public static void main(String[] args) {
+		System.out.println("==>main");
+		System.out.println("<==main");
+	}
+
+}
